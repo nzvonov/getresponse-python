14ba9fa32655280fa578f6b42a479512335bf60f
Nikolay Zvonov <nikzvonov@gmail.com>
2020-04-09 11:57:45 +1000

    refactored GetResponse.__prepare_params; version up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 
 setup(name='getresponse-python',
-      version='0.2.0',
+      version='0.2.1',
       description='Email Marketing Software',
       long_description=read('README.md'),
       long_description_content_type="text/markdown",
