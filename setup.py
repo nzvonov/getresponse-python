fa3a044dae8960d977b658ce43f0f991df15998a
Nikolay Zvonov <nikzvonov@gmail.com>
2020-04-09 11:40:31 +1000

    version up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 
 setup(name='getresponse-python',
-      version='0.1.2',
+      version='0.2.0',
       description='Email Marketing Software',
       long_description=read('README.md'),
       long_description_content_type="text/markdown",
