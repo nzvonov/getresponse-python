6b1163a108977017dfc0ddd4bc968dc604e9d4cf
Nikolay Zvonov <nzvonov@infodev.ru>
2021-01-22 15:45:54 +1000

    update docstring snake_to_camel util;

--- getresponse/utils.py
+++ getresponse/utils.py
@@ -4,11 +4,14 @@
 def snake_to_camel(snake_string):
     """
     Format string from snake_case to camelCase.
-    :param snake_string: string in snake_case format
-    :return: string in camelCase format
+
+    Args:
+        snake_string: string in snake_case format.
+
+    Returns:
+        return: string in camelCase format
     """
     splitter = '_'
     split_field = snake_string.split(splitter)
     titled_words = [word.title() for word in split_field[1:]]
-    camel_string = ''.join(split_field[:1] + titled_words)
-    return camel_string
+    return ''.join(split_field[:1] + titled_words)
